0b525dbc111e866ee087e473dc4780c88fee1e78
Seok-Jin Kim <suckjin4013@gmail.com>
2025-07-03 15:16:07 +0900

    Delete migrations directory

--- migrations/1_deploy_contracts.js
+++
@@ -1,9 +0,0 @@
-const Migrations = artifacts.require("Migrations");
-const MyNFTs = artifacts.require("MyNFTs");  // MyNFTs.sol 계약 추가
-
-module.exports = function (deployer) {
-  deployer.deploy(Migrations)
-    .then(() => {
-      return deployer.deploy(MyNFTs);  // MyNFTs.sol 계약 배포
-    });
-};
