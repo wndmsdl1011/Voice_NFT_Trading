b10d536a0e644929ec99da3f4235b198e73ddda6
elevensheep <fttc100@gmail.com>
2025-07-09 01:21:17 +0900

    update router

--- src/backend/node/NFT/routes/nft.routes.js
+++ src/backend/node/NFT/routes/nft.routes.js
@@ -3,4 +3,8 @@ const router = express.Router();
 
 const nftController = require('../controllers/nft.controller');
 
+// NFT 민팅 라우트
 router.post('/mint', nftController.mintNFT);
+
+// 반드시 export 해줘야 외부에서 사용 가능
+module.exports = router;
